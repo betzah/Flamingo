83b53ed31df3918588e580b8d5aa8a1c474eef30
lorenzo456 <lorenzo456_@hotmail.com>
2016-01-29 22:55:40 +0100

    FlamingoSpawner

---
+++ Assets/SpawnFlamingos.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFlamingos : MonoBehaviour {
+
+    public int numberOfEnemies; 
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            if ( i % 2 == 0)
+            {
+                Instantiate(Resources.Load("Flamingo"), new Vector3(i * 2, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(Resources.Load("Flamingo"), new Vector3(i * 2, 0, 1), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
