1df8eb47b3b2af17e812cbf4208764f23455148d
emielboven <emielboven@gmail.com>
2016-01-30 14:38:42 +0100

    MenuScript

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.anyKeyDown) {
-			LoadScene (1);
+
+			GetComponent<Animation>().Play();
+
+			SceneManager.LoadScene (1);
 		}
 	}
 
-	public void LoadScene() ;
-}
+
+
+}      
