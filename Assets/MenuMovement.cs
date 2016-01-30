b26f399df2bcb9eeb3dbd9a1b1f7b83fc414a52e
emielboven <emielboven@gmail.com>
2016-01-30 20:56:37 +0100

    ROCKS!!!!

---
+++ Assets/MenuMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMovement : MonoBehaviour {
+	public Transform Target1;
+	public Transform Target2;
+	public string Target;
+	public float WalkSpeed;
+	// Use this for initialization
+	void Start () {
+		Target = ("Target1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+		if (transform.position == Target1.position) {
+			Target = ("Target2");
+
+		}
+
+		if (transform.position == Target2.position) {
+			Target = ("Target1");
+
+		}
+
+		if (Target == ("Target1")) {
+			transform.position = Vector3.MoveTowards (transform.position, Target1.position, WalkSpeed* Time.deltaTime);
+			transform.LookAt(Target1.position);
+
+		}
+
+		if (Target == ("Target2")) {
+			transform.position = Vector3.MoveTowards(transform.position, Target2.position, WalkSpeed*Time.deltaTime);
+			transform.LookAt(Target2.position);
+		}
+	}
+}
