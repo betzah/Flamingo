e6ec11db5e0535d1afc034a9627e8a8a5820ffce
lorenzo456 <lorenzo456_@hotmail.com>
2016-01-30 02:23:02 +0100

    spawnflamingo

--- Assets/SpawnFlamingos.cs
+++ Assets/SpawnFlamingos.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnFlamingos : MonoBehaviour {
 
-    public int numberOfEnemies; 
+    List<GameObject> listOfFlamingos = new List<GameObject>();
+
+    public int numberOfEnemies;
+    public float depth;
 
 	// Use this for initialization
 	void Start () {
         for (int i = 0; i < numberOfEnemies; i++)
         {
-            if ( i % 2 == 0)
-            {
-                Instantiate(Resources.Load("Flamingo"), new Vector3(i * 2, 0, 0), Quaternion.identity);
-            }
-            else
-            {
-                Instantiate(Resources.Load("Flamingo"), new Vector3(i * 2, 0, 1), Quaternion.identity);
-            }
+                GameObject clone = Instantiate(Resources.Load("Flamingo"), new Vector3(i * 2, 0, (i % 2) * depth), Quaternion.identity) as GameObject;
+            listOfFlamingos.Add(clone);
         }
+
+        Destroy(listOfFlamingos[numberOfEnemies / 2].GetComponent<Dummy>());
+        listOfFlamingos[numberOfEnemies / 2].AddComponent<Player>();
 	}
 	
 	// Update is called once per frame
