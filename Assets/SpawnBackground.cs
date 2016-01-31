5a31f3a2429375ca145e4d431d8bea1ec4cb076e
lorenzo456 <lorenzo456_@hotmail.com>
2016-01-31 12:42:48 +0100

    water

--- Assets/SpawnBackground.cs
+++ Assets/SpawnBackground.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class SpawnBackground : MonoBehaviour {
 
-	float backgroundSpeed = 2.0f;
-
+	float backgroundSpeed = 1.0f;
+    public GameObject planeHolder;
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < 6; i++)
+        planeHolder = GameObject.FindGameObjectWithTag("holder");
+        for (int i = 0; i < 9; i++)
         {
             float randomNum = Random.Range(0f, 1f);
             int randomObj = Random.Range(1, 7);
-            if (randomNum > 0.5f)
+            if (randomNum > 0.2f)
             {
               GameObject clone = Instantiate(Resources.Load("BackGround/" + randomObj), this.gameObject.transform.GetChild(i).transform.position, Quaternion.Euler(0,-90,0)) as GameObject;
                 clone.transform.SetParent(this.transform);
@@ -27,10 +28,12 @@ public class SpawnBackground : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position -= new Vector3(backgroundSpeed, 0, 0) * Time.deltaTime;
-        if(this.transform.position.x <= -29)
+        if(this.transform.position.x <= -10)
         {
-			Instantiate(Resources.Load("Ground"), new Vector3(61, -0.01f, 3), Quaternion.identity);
+			GameObject clone = Instantiate(Resources.Load("WaterPlane"), new Vector3(40, -0.01f, 3), Quaternion.identity) as GameObject;
+            clone.transform.parent = planeHolder.transform;
             Destroy(this.gameObject);
+            Debug.Log("destroy myself");
         }
 	}
 
